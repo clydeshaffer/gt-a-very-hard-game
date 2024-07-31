59eb96cccecb9cfbf108c3018ab894e55aa219f4
Clyde Shaffer <clydeshaffer@gmail.com>
2024-07-30 20:47:12 -0400

    rename stuff for merge

---
+++ src/combo_main.c
@@ -0,0 +1,60 @@
+#include "gt/gametank.h"
+#include "gt/banking.h"
+#include "gt/drawing_funcs.h"
+#include "gt/input.h"
+#include "gt/dynawave.h"
+#include "gt/music.h"
+#include "gt/feature/text/text.h"
+#include "gen/assets/menu.h"
+extern void CubicleReset();
+extern int avhg_main();
+char selection = 0;
+
+#pragma code-name(push, "PROG1")
+void drawbg() {
+    clear_border(32);
+    draw_sprite(1, 1, 126, 126, 1, 1, 0);
+    await_draw_queue();
+    print_text(">");
+    await_drawing();
+}
+#pragma code-name(pop)
+
+int main() {
+    init_graphics();
+    init_dynawave();
+    init_music();
+    change_rom_bank(0xFC);
+    load_spritesheet(&ASSET__menu__menuscreen_bmp, 0);
+    load_font(1);
+    init_text();
+
+    text_cursor_x = 21;
+    text_cursor_y = 16;
+    text_color = TEXT_COLOR_WHITE;
+    drawbg();
+    
+    flip_pages();
+    text_cursor_x = 26;
+    text_cursor_y = 105;
+    text_color = TEXT_COLOR_BLACK;
+    drawbg();
+    while (!(selection & 2))
+    {
+        update_inputs();
+        if(player1_new_buttons & (INPUT_MASK_UP | INPUT_MASK_DOWN)) {
+            flip_pages();
+            selection = !selection;
+        }
+        if(player1_new_buttons & (INPUT_MASK_START | INPUT_MASK_A)) {
+            selection |= 2;
+        }
+        sleep(1);
+    }
+    
+    if(selection & 1) {
+        avhg_main();
+    } else {
+        CubicleReset();
+    }
+}
